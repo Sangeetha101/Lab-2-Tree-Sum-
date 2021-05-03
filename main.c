6d3e6e148dcad335b19b431c825a412a7f10a9f0
Sangeetha101 <83044139+Sangeetha101@users.noreply.github.com>
2021-05-02 19:33:37 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,68 +1,117 @@
-#include<stdio.h>
-#include<omp.h>
-
-
-
-main()
-{
-	float          *Array, *Check, serial_sum, sum, partialsum;
-	int             array_size, i;
-
-	printf("Enter the size of the array\n");
-	scanf("%d", &array_size);
-
-	if (array_size <= 0) {
-		printf("Array Size Should Be Of Positive Value ");
-		exit(1);
-	}
-	/* Dynamic Memory Allocation */
-
-	Array = (float *) malloc(sizeof(float) * array_size);
-	Check = (float *) malloc(sizeof(float) * array_size);
-
-	/* Array Elements Initialization */
-
-	for (i = 0; i < array_size; i++) {
-		Array[i] = i * 5;
-		Check[i] = Array[i];
-	}
-
-	printf("The Array Elements Are \n");
-
-	for (i = 0; i < array_size; i++)
-		printf("Array[%d]=%f\n", i, Array[i]);
-
-	sum = 0.0;
-	partialsum = 0.0;
-
-	/* OpenMP Parallel For Directive And Critical Section */
-
-#pragma omp parallel for shared(sum)
-	for (i = 0; i < array_size; i++) {
-#pragma omp critical
-		sum = sum + Array[i];
-
-	}
-
-	serial_sum = 0.0;
-
-	/* Serail Calculation */
-	for (i = 0; i < array_size; i++)
-		serial_sum = serial_sum + Check[i];
-
-
-	if (serial_sum == sum)
-		printf("\nThe Serial And Parallel Sums Are Equal\n");
-	else {
-		printf("\nThe Serial And Parallel Sums Are UnEqual\n");
-		exit(1);
-	}
-
-	/* Freeing Memory */
-	free(Check);
-	free(Array);
-
-	printf("\nThe SumOfElements Of The Array Using OpenMP Directives Is %f\n", sum);
-	printf("\nThe SumOfElements Of The Array By Serial Calculation Is %f\n", serial_sum);
-}
+#include <mpi.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+  
+// size of array 
+#define n 10 
+  
+int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; 
+  
+// Temporary array for slave process 
+int a2[1000]; 
+  
+int main(int argc, char* argv[]) 
+{ 
+  
+    int pid, np, 
+        elements_per_process, 
+        n_elements_recieved; 
+    // np -> no. of processes 
+    // pid -> process id 
+  
+    MPI_Status status; 
+  
+    // Creation of parallel processes 
+    MPI_Init(&argc, &argv); 
+  
+    // find out process ID, 
+    // and how many processes were started 
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid); 
+    MPI_Comm_size(MPI_COMM_WORLD, &np); 
+  
+    // master process 
+    if (pid == 0) { 
+        int index, i; 
+        elements_per_process = n / np; 
+  
+        // check if more than 1 processes are run 
+        if (np > 1) { 
+            // distributes the portion of array 
+            // to child processes to calculate 
+            // their partial sums 
+            for (i = 1; i < np - 1; i++) { 
+                index = i * elements_per_process; 
+  
+                MPI_Send(&elements_per_process, 
+                         1, MPI_INT, i, 0, 
+                         MPI_COMM_WORLD); 
+                MPI_Send(&a[index], 
+                         elements_per_process, 
+                         MPI_INT, i, 0, 
+                         MPI_COMM_WORLD); 
+            } 
+  
+            // last process adds remaining elements 
+            index = i * elements_per_process; 
+            int elements_left = n - index; 
+  
+            MPI_Send(&elements_left, 
+                     1, MPI_INT, 
+                     i, 0, 
+                     MPI_COMM_WORLD); 
+            MPI_Send(&a[index], 
+                     elements_left, 
+                     MPI_INT, i, 0, 
+                     MPI_COMM_WORLD); 
+        } 
+  
+        // master process add its own sub array 
+        int sum = 0; 
+        for (i = 0; i < elements_per_process; i++) 
+            sum += a[i]; 
+  
+        // collects partial sums from other processes 
+        int tmp; 
+        for (i = 1; i < np; i++) { 
+            MPI_Recv(&tmp, 1, MPI_INT, 
+                     MPI_ANY_SOURCE, 0, 
+                     MPI_COMM_WORLD, 
+                     &status); 
+            int sender = status.MPI_SOURCE; 
+  
+            sum += tmp; 
+        } 
+  
+        // prints the final sum of array 
+        printf("Sum of array is : %d\n", sum); 
+    } 
+    // slave processes 
+    else { 
+        MPI_Recv(&n_elements_recieved, 
+                 1, MPI_INT, 0, 0, 
+                 MPI_COMM_WORLD, 
+                 &status); 
+  
+        // stores the received array segment 
+        // in local array a2 
+        MPI_Recv(&a2, n_elements_recieved, 
+                 MPI_INT, 0, 0, 
+                 MPI_COMM_WORLD, 
+                 &status); 
+  
+        // calculates its partial sum 
+        int partial_sum = 0; 
+        for (int i = 0; i < n_elements_recieved; i++) 
+            partial_sum += a2[i]; 
+  
+        // sends the partial sum to the root process 
+        MPI_Send(&partial_sum, 1, MPI_INT, 
+                 0, 0, MPI_COMM_WORLD); 
+    } 
+  
+    // cleans up all MPI state before exit of process 
+    MPI_Finalize(); 
+  
+    return 0; 
 }
